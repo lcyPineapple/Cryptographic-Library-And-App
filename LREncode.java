14c9d833b4ad5252217f27207dfb4d01f2332914
BuildTools <unconfigured@null.spigotmc.org>
2021-04-24 20:17:24 -0700

    skeleton code

---
+++ LREncode.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+
+public class LREncode {
+    
+    public static Array[] rightEncode(Array[] s){
+        return s;
+        }
+
+        public static Array[] leftEncode(Array[] s){
+            return s;
+            }    
+
+}
+
---
+++ bytepadES.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Array;
+
+public class bytepadES {
+
+    public static Array[] bytepad(Array[] s){
+        return s;
+        }
+
+        public static String encodeString(String s){
+            return s;
+            }
+    
+}
