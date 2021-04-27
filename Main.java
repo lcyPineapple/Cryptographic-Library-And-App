ee03e5b14c9728a18eab05d80a947b78e1e5c0c2
lcyPineapple <56752219+lcyPineapple@users.noreply.github.com>
2021-04-27 15:09:25 -0700

    updates to right Encode

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
+import java.math.BigInteger;
 class Main {
     public static void main(String[] args) {
-        byte[] bytes = LREncode.enc8(128);
-        System.out.println(bytes[0]);
+        /////////////////////////////////////
+        //Test Section for LREncode:
+        //byte[] bytes = LREncode.enc8(128);
+        //System.out.println(bytes[0]);
+        BigInteger myInt = new BigInteger("127");
+        byte[] bytes = LREncode.rightEncode(myInt);
+        ////////////////////////////////////
+
     }
 }
